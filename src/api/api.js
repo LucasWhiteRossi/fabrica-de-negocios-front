6da3e5cd87f46d7fd277247a88d3178a3b343836
Lucas White Rossi <lucaswrossi@gmail.com>
2022-05-13 11:59:15 -0300

    changing production variable on api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiURLs = {
   development: "http://localhost:4000",
-  production: "COLOQUE AQUI O LINK DA SUA API DEPLOYADA",
+  production: "https://fabricadenegocios.herokuapp.com/",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
