f2c889fc818746cfa7586c796c0448fd49edeef9
unknown <renatabssiqueira@gmail.com>
2022-05-20 14:20:47 +0100

    navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,6 +4,7 @@ import { AuthContext } from "../../contexts/authContext";
 import { Link } from "react-router-dom";
 import { DropdownButton, Dropdown }  from 'react-bootstrap'
 import DropdownItem from 'react-bootstrap/esm/DropdownItem';
+import logoImg from "./../../assets/img/nav_bar.png"
 
 
 export function Navbar() {
@@ -13,7 +14,13 @@ export function Navbar() {
   return (
     <nav class="navbar" style={{"backgroundColor":"#6610f2"}}>
    
-   <h1><Link to="/" style={{ textDecoration: "none", color:"white"}}>FÁBRICA DE NEGÓCIOS</Link></h1>
+   <h1>
+    <Link to="/" style={{ textDecoration: "none" }}>
+      <div style={{color:"#713e9b"}}>
+        <img src={logoImg} alt="Fábrica de Personas" style={{width:"400px",height:"auto"}}/>
+      </div>
+      </Link>
+    </h1>
     
     {!loggedInUser &&(
     <div class="nav-items">
